60accdf34efea73316b451a1e3c54d553fd8cd61
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-03-26 17:40:12 +0100

    created skeleton for a bounded buffer

---
+++ FileServer/BoundedBuffer.cpp
@@ -0,0 +1,28 @@
+#include "BoundedBuffer.h"
+
+BoundedBuffer::~BoundedBuffer(void)
+{
+}
+
+void BoundedBuffer::put(tcp::socket element) {
+	freeElements.wait();
+	{
+		boost::mutex::scoped_lock lock(queueMutex);
+		queue.push_back(&element);
+	}
+	availableElements.post();
+}
+
+tcp::socket BoundedBuffer::get(void) {
+	tcp::socket* element;
+
+	availableElements.wait();
+	{
+		boost::mutex::scoped_lock lock(queueMutex);
+		element = queue.front();
+		queue.pop_front();
+	}
+	freeElements.post();
+
+	return *element;
+}
---
+++ FileServer/BoundedBuffer.h
@@ -0,0 +1,27 @@
+#ifndef BOUNDEDBUFFER_H_
+#define BOUNDEDBUFFER_H_
+
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <deque>
+#include <iostream>
+#include <boost/interprocess/sync/interprocess_semaphore.hpp>
+
+using boost::asio::ip::tcp;
+using namespace std;
+
+class BoundedBuffer
+{
+public:
+	BoundedBuffer(int maxElements) : availableElements(0), freeElements(maxElements) {}
+	~BoundedBuffer(void);
+	void put(tcp::socket element);
+	tcp::socket get(void);
+
+private:
+	deque<tcp::socket*> queue;
+	boost::mutex queueMutex;
+	boost::interprocess::interprocess_semaphore availableElements, freeElements;
+};
+
+#endif
