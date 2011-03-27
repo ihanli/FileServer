bd2a3ec513c9add574014300954fcb87510cc65d
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-03-27 15:50:00 +0200

    implemented run method of consumer (not tested)

--- FileServer/Consumer.cpp
+++ FileServer/Consumer.cpp
@@ -4,29 +4,38 @@ void Consumer::run(void)
 {
 	while(true)
 	{
-		tcp::socket socket = connectedSockets->get();
+		tcp::socket* socket = connectedSockets->get();
 		string method, filename;
-		char requestBuffer[requestBufferSize];
-		int requestSize = socket.receive(boost::asio::buffer(requestBuffer, requestBufferSize));
+		char requestBuffer[1024];
+		int requestSize = socket->receive(boost::asio::buffer(requestBuffer, 1024));
 		stringstream httpRequest(string(requestBuffer, requestSize));
 
 		httpRequest >> method >> filename;
 
 		//TODO: continue if method is get
 
-		//TODO: open file and determine size
+		stringstream payload;
+		string singleLine;
+		requestedFile.open(FILE_PATH + filename);
+		
+		while(getline(requestedFile, singleLine))
+		{
+			payload << singleLine;
+		}
+
+		requestedFile.close();
 
 		std::stringstream httpResponse;
 		httpResponse << "HTTP/1.0 200 OK\n";
 		httpResponse << "Server: FileServer/0.0.1\n";
 		httpResponse << "Content-Type: application/octet-stream\n";
-		httpResponse << "Content-Length: " << payloadSize << "\n\n";
+		httpResponse << "Content-Length: " << payload.str().length() << "\n\n";
 		// send the http-response header
-		socket.send(boost::asio::buffer(httpResponse.str().c_str(), httpResponse.str().length()));
+		socket->send(boost::asio::buffer(httpResponse.str().c_str(), httpResponse.str().length()));
 
 		// send the http-response payload
-		socket.send(boost::asio::buffer(payload.c_str(), payloadSize));
-		socket.shutdown(tcp::socket::shutdown_both);
-		socket.close();
+		socket->send(boost::asio::buffer(payload.str().c_str(), payload.str().length()));
+		socket->shutdown(tcp::socket::shutdown_both);
+		socket->close();
 	}
 }
--- FileServer/Consumer.h
+++ FileServer/Consumer.h
@@ -2,17 +2,22 @@
 #define CONSUMER_H_
 
 #include "BoundedBuffer.h"
+#include <fstream>
+
+using namespace std;
 
 class Consumer
 {
 public:
-	Consumer(BoundedBuffer* connectedSockets) { this->connectedSockets = connectedSockets; }
+	Consumer(BoundedBuffer* connectedSockets) : FILE_PATH("files/") { this->connectedSockets = connectedSockets; }
 	~Consumer(void);
 	void run(void);
 
 private:
 	BoundedBuffer* connectedSockets;
-	const unsigned int requestBufferSize = 1024;
+	ifstream requestedFile;
+	//const int REQUEST_BUFFER_SIZE;
+	const string FILE_PATH;
 };
 
 #endif
